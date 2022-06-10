0a1c75ab426444ff4de9d0e3f26e87fd6252b5b3
Elfateru <105721309+Elfateru@users.noreply.github.com>
2022-06-10 17:43:35 +0300

    job done

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def least_divisor(num):
+    temp = 0
+    for count in range(num, 1, -1):
+        if num % count == 0:
+            temp = count
+    if temp == 0:
+        temp = num
+    return temp
+
+
+number = int(input('Введите число: '))
+
+print('Наименьший делитель, отличный от единицы: ', least_divisor(number))
